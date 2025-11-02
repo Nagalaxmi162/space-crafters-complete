6f4a59327370bf5ee1b4ef802b1cb6bef5f4edbd
Nagalaxmi162 <lenkalanagakshmi719@gmail.com>
2025-11-02 19:28:37 +0530

    2nd commit

--- app.js
+++ app.js
@@ -76,3 +76,8 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
